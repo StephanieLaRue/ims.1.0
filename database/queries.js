c2c1de70447e533c9b213ee581a2807f7cd4d706
stephanie <stephlarue26@gmail.com>
2019-05-04 16:26:43 -0700

    adding sold status to items

--- database/queries.js
+++ database/queries.js
@@ -1,5 +1,6 @@
 const pg = require("pg")
 const insertNewData = "INSERT INTO userdata(date, description, brand, cost, style, size, color, pattern) values($1, $2, $3, $4, $5, $6, $7, $8)";
+const insertSoldData = "INSERT INTO userdata(sold) values($9)";
 const creds = require("../config.json")
 
 let config = {
@@ -17,7 +18,8 @@ module.exports = {
     
     addData: function(parsedInput) {
 
-        let values = [parsedInput.date, parsedInput.description, parsedInput.brand, parsedInput.cost, parsedInput.style, parsedInput.size, parsedInput.color, parsedInput.pattern]
+        let values = [parsedInput.date, parsedInput.description, parsedInput.brand, parsedInput.cost, 
+            parsedInput.style, parsedInput.size, parsedInput.color, parsedInput.pattern]
         
         pool.on('error', (err) => {
             console.error('Idle Client...', err)
@@ -29,7 +31,8 @@ module.exports = {
                 console.log('Connection Error in "addData"... ' + err) 
                 return
             }
-            
+
+           
             client.query(insertNewData, values, function (err, result) {
                 console.log("added query ", result);
                 
@@ -56,6 +59,31 @@ module.exports = {
                 if(err) callback(err)
                 let json = JSON.stringify(result)
                 callback(null, json)
+            })
+        })
+    },
+    addSoldStatus: function(parsedInput) {
+
+        let values = [parsedInput.soldStatus]
+        console.log(values);
+         return
+        
+        pool.on('error', (err) => {
+            console.error('Idle Client...', err)
+            process.exit(1)
+        })
+
+        pool.connect((err, client, done) => {
+            if(err) { 
+                console.log('Connection Error in "addstatus"... ' + err) 
+                return
+            }
+           
+            client.query(insertSoldData, values, function (err, result) {
+                console.log("added sold query ", result);
+                
+                done()
+                if (err) throw new Error(err);
             })
         })
     }
--- source/inventory.jsx
+++ source/inventory.jsx
@@ -4,24 +4,57 @@ import './index.scss';
 //Calendar
 import 'flatpickr/dist/themes/material_blue.css'
 
+
 class Inventory extends React.Component {
 
 	constructor() {
 		super();
 	
 		this.state = {
-          date: new Date(),
-          rawInventory: [],
-          cleanInventory: []
+            date: new Date(),
+            rawInventory: [],
+            cleanInventory: [],
+            // soldItem: "red",
+            soldStatus: false
         };
         
         this.viewInventory = this.viewInventory.bind(this)
         this.getInventory = this.getInventory.bind(this)
+        this.handleSoldClick = this.handleSoldClick.bind(this);
     }
       
-      componentDidMount() {
+    componentDidMount() {
         this.getInventory()
     }
+
+    handleSoldClick() {
+        this.setState({
+            soldStatus: this.state.soldStatus = true
+        });
+
+        console.log(this.state.soldStatus);
+
+        let sold = { soldStatus: this.state.soldStatus }
+        
+        let params = {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(sold)
+          }
+          fetch(`${location.origin}/sold-item/status`, params)
+        //   .then(res => res.json())
+        //   .then((data) => {
+            // this.setState({rawInventory: data.rows}, () => {
+            //   this.viewInventory(this.state.rawInventory)
+            // })  
+        //   })
+        //   .catch(error => console.error('Error GETTING Data:', error))
+      }
+
+    
 
     getInventory() {
         let params = {
@@ -47,10 +80,16 @@ class Inventory extends React.Component {
         
         let inventory = data.map((item, ind) => {
 
+            let soldBtn = <div id="soldBtn" onClick={this.handleSoldClick} style={{backgroundColor: this.state.soldItem}}> </div>
+
             return (
 
                     <tr className="list-item">
-                        <th className="list-item-text" colSpan="100">Item: 1234</th>
+                        <th className="header-row" colspan="100">
+                            <div className="list-item-text" >Item: 1234</div>
+                            {soldBtn}
+                        </th>
+                        
                         <tr className="data-row">
                             <th className="table-headers">Date</th>
                             <td className="table-data">{item.date}</td>
